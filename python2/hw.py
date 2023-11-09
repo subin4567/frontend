5576d969e0426e943c2122ed1de90db038cef8f2
subin4567 <subinram17@gmail.com>
2023-11-09 12:10:59 +0530

    new

--- python2/hw.py
+++ python2/hw.py
@@ -16,15 +16,15 @@
 # print(area)
 
 # 3
-def swap():
-    a=5
-    b=10
-    a=x
-    b=y
-    x=b
-    y=a
-    print(x)
-swap()
+# def swap():
+#     a=5
+#     b=10
+#     a=x
+#     b=y
+#     x=b
+#     y=a
+#     print(x)
+# swap()
 
     
 
@@ -103,5 +103,143 @@ swap()
 # print(average)
 
 # 11
+
+# def cal():
+
+    
+#     a=float(input("enter the first decimal"))
+#     b=float(input("enter the second decimal"))
+#     c=float(input("enter the second decimal"))
+
+#     total=(a+b+c)
+#     print(total)
+#     average=(total/2)
+#     print(average)
+# cal()
+
+
+# day 2
+# 1
+# a=[1,2,3,4,5]
+# for i in a:
+#     print(i)
+
+    # 2
+
+# for i in range (1,11):
+#     print(i)
+
+# even=[]
+# for i in range(2,21):
+    
+#     if i%2==0:
+#         # print(i,"even")
+#         even.append(i)
+# print(even)
+
+# 4
+# a=["subin","kumush","sree","ram"]
+# for i in range(len(a)):
+#     print(a[i])
+ 
+
+# 5
+# for i in range(1,30):
+#     if i%2!=0:
+#         print(i)
+    
+    # 6
+
+# def vote():
+#     age=int(input("enter your age"))
+#     if age>=18:
+#         print("you are eligible to vote")
+#     else:
+#         print("you are not eligible to vote")
+# vote()
+
+# 7
+
+
+
+# a=[12,4,3]
+
+# for i in range(0):
+#     print(a[i])
+
+
+# def odd():
+#     a=int(input("enter the age"))
+#     for i in range(1,a+1):
+#         if i%2!=0:
+#          print(i)
+# odd()
+
+# 8
+
+# 9
+# a=[1,2,3,4,5,6]
+# c=2
+# a. remove(c)
+# print(a)
+
+
+    #    day3
+# 1
+
+
+# number = float(input("Enter a number: "))
+# result = (number)
+
+# print(f"The square of {number} is {result:.2f}")
+
+# day 4
+
+# 8
+
+# a=[1,3,3,4,3,2,3]
+# n=3
+# count=0
+# for i in a:
+#     if i==n:
+#         count=count+1
+# print(count,"times")
+
+# 9
+# a="selva kunsh is dating with selva kumar selva"
+# c="selva"
+# count=0
+
+# list=a.split()
+# # print(list)
+# for i in list:
+#     if i==c:
+#         count=count+1
+# print(count,"times selva in a paragraph")
+
+# factorial
+
+
+# num=5
+
+# count=0
+# for i in range(num,0,-1):
+#     count = count+i
+
+# print(count)
+
+a=int(input("enter numb"))
+b=int(input("enter numb2"))
+c=(input("enter numb3"))
+if c=="+":
+    x=(a+b)
+    print(x)
+
+
+
+
+
+
+
 
 
